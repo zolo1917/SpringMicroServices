5377610a79379659496f0f179cd2866190a497db
zolo1917 <adwaith_venkatesh@msn.com>
2019-11-12 12:17:34 +0530

    Adding zuul api gateway to webApplication

--- limit-service/src/main/java/com/microservices/demo/controller/LimitServiceRestController.java
+++ limit-service/src/main/java/com/microservices/demo/controller/LimitServiceRestController.java
@@ -17,37 +17,39 @@ import com.netflix.discovery.shared.Application;
 
 @RestController
 public class LimitServiceRestController {
-	
+
 	@Autowired
 	private limitsConfig config;
-	
+
 	@Autowired
 	private EurekaClient eurekaClient;
-	
+
 	private final RestTemplate restTemplate;
-	
+
 	@Value("${eureka.DataBaseService.serviceId}")
 	private String zipCodeSearchId;
-	
+
 	LimitServiceRestController(RestTemplateBuilder restTemplateBuilder) {
-		this.restTemplate = restTemplateBuilder.build(); 
+		this.restTemplate = restTemplateBuilder.build();
 	}
-	
+
 	@GetMapping("/limitConfiguration")
 	public LimitConfiguration limitconfig() {
 		return new LimitConfiguration(config.getMaxium(), config.getMinium());
 	}
-	
+
 	@GetMapping("/getCityState/{zipCode}")
 	public ZipCodeModel getzipStateDetails(@PathVariable Long zipCode) {
-        Application application = eurekaClient.getApplication(zipCodeSearchId);
+		Application application = eurekaClient.getApplication(zipCodeSearchId);
 
-        InstanceInfo instanceInfo = application.getInstances().get(0);
+		InstanceInfo instanceInfo = application.getInstances().get(0);
 
-        String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/" + "/getCityState/"+zipCode.toString();
+		String url = "http://" + instanceInfo.getIPAddr() + ":" + instanceInfo.getPort() + "/" + "/getCityState/"
+				+ zipCode.toString();
 
-        System.out.println("URL" + url);
-		ZipCodeModel zip = this.restTemplate.getForObject("http://localhost:8001/getCityState/{zipCode}", ZipCodeModel.class,zipCode);
+		System.out.println("URL" + url);
+		ZipCodeModel zip = this.restTemplate.getForObject("http://localhost:8001/getCityState/{zipCode}",
+				ZipCodeModel.class, zipCode);
 		return zip;
 	}
 }
